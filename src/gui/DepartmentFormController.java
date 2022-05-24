9e91d946889bcd13fda4b706fa946fb16edc8d6a
JRamos29 <malebolge@gmail.com>
2022-05-24 10:53:04 -0300

    DepartmentForm controller

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,51 +1,39 @@
-//package gui;
-//
-//import java.net.URL;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.ResourceBundle;
-//import java.util.Set;
-//
-//import db.DbException;
-//import gui.listeners.DataChangeListener;
-//import gui.util.Alerts;
-//import gui.util.Constraints;
-//import gui.util.Utils;
-//import javafx.event.ActionEvent;
-//import javafx.fxml.FXML;
-//import javafx.fxml.Initializable;
-//import javafx.scene.control.Alert.AlertType;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.Label;
-//import javafx.scene.control.TextField;
-//import model.entities.Department;
-//import model.exceptions.ValidationException;
-//import model.services.DepartmentService;
-//
-//public class DepartmentFormController implements Initializable {
-//
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable {
+
 //	private Department entity;
 //	
 //	private DepartmentService service;
 //	
 //	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 //	
-//	@FXML
-//	private TextField txtId;
-//	
-//	@FXML
-//	private TextField txtName;
-//	
-//	@FXML
-//	private Label labelErrorName;
-//	
-//	@FXML
-//	private Button btSave;
-//	
-//	@FXML
-//	private Button btCancel;
-//	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
 //	public void setDepartment(Department entity) {
 //		this.entity = entity;
 //	}
@@ -57,9 +45,9 @@
 //	public void subscribeDataChangeListener(DataChangeListener listener) {
 //		dataChangeListeners.add(listener);
 //	}
-//	
-//	@FXML
-//	public void onBtSaveAction(ActionEvent event) {
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
 //		if (entity == null) {
 //			throw new IllegalStateException("Entity was null");
 //		}
@@ -78,8 +66,8 @@
 //		catch (DbException e) {
 //			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 //		}
-//	}
-//	
+	}
+	
 //	private void notifyDataChangeListeners() {
 //		for (DataChangeListener listener : dataChangeListeners) {
 //			listener.onDataChanged();
@@ -104,22 +92,22 @@
 //		
 //		return obj;
 //	}
-//
-//	@FXML
-//	public void onBtCancelAction(ActionEvent event) {
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
 //		Utils.currentStage(event).close();
-//	}
-//	
-//	@Override
-//	public void initialize(URL url, ResourceBundle rb) {
-//		initializeNodes();
-//	}
-//	
-//	private void initializeNodes() {
-//		Constraints.setTextFieldInteger(txtId);
-//		Constraints.setTextFieldMaxLength(txtName, 30);
-//	}
-//	
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
 //	public void updateFormData() {
 //		if (entity == null) {
 //			throw new IllegalStateException("Entity was null");
@@ -135,4 +123,4 @@
 //			labelErrorName.setText(errors.get("name"));
 //		}
 //	}
-//}
+}
